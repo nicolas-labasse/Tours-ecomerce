85b008ff0fab4817a63a129fe1a12bda2064793d
nicolas-labasse <nicolaslabasse4@gmail.com>
2023-06-24 18:09:41 -0300

    Push reserva
    funcion para reservar

---
+++ codigo.js
@@ -0,0 +1,10 @@
+function reserva(nombre, apellido, dni, habitacion, fechaEntrada, fechaSalida) {
+this.nombre = nombre;
+this.apellido = apellido;
+this.dni = dni;
+this.habitacion = habitacion;
+this.fechaEntrada = fechaEntrada;
+this.fechaSalida = fechaSalida;
+}
+
+const reservas = [];
